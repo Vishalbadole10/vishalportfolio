911f59446f4682bf7f45c1e79002224f50542c6a
Vishalbadole10 <140186563+Vishalbadole10@users.noreply.github.com>
2023-07-24 09:05:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("hello");
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburg').addEventListener("click", () => {
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.ham').style.display = 'inline';
+        document.querySelector('.cross').style.display = 'none';
+    }
+    else{
+        document.querySelector('.ham').style.display = 'none';
+       
+        setTimeout(()=>{
+            document.querySelector('.cross').style.display = 'inline';
+    },400 );
+}
+    })
+    
+            function startTyping() {
+                var typed = new Typed(".typed-text", {
+                    strings: ["Goutam Kushwah.", "Coder.", "Web developer."],
+                    typeSpeed: 200,
+                    backSpeed: 200,
+                    loop: true
+                });
+            }
+    
+            // Start the typing animation when the page is loaded
+            document.addEventListener("DOMContentLoaded", startTyping);
+        
